ba3932f59679e892efdf57383143b876e2e404b7
saikirangan <saikiran@popup-mainframe.com>
2024-10-28 12:02:54 +0530

    Updated Hellopgm

--- COB/HELLOPGM.cob
+++ COB/HELLOPGM.cob
@@ -4,6 +4,5 @@
        DATA DIVISION.                                                   00040001
        PROCEDURE DIVISION.                                              00060001
            DISPLAY 'HELLO WORLD'.                                       00070001
-           DISPLAY 'POPUP_india'.
            DISPLAY 'CODE UPDATE'
            STOP RUN.                                                    00080001
