17c987ea77235af616fe472d20d0cdc4bac802f8
saikirangan <135574001+saikirangan@users.noreply.github.com>
2025-02-11 17:13:46 +0530

    Update ADDPGM.cob

--- COB/ADDPGM.cob
+++ COB/ADDPGM.cob
@@ -1,21 +1,20 @@
-       IDENTIFICATION DIVISION.                                         00010001
-       PROGRAM-ID. ADDPGM.                                              00020001
-       ENVIRONMENT DIVISION.                                            00030001
-       DATA DIVISION.                                                   00040001
-       WORKING-STORAGE SECTION.                                         00040002
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. ADDPGM.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
        01 WS-X    PIC 9(02).
        01 WS-Y    PIC 9(02).
        01 WS-Z    PIC 9(03).
        01 WS-TOT  PIC 9(03) .
-       PROCEDURE DIVISION.                                              00060001
+       PROCEDURE DIVISION.
            ACCEPT WS-X.
            ACCEPT WS-Y.
            ACCEPT WS-Z.
-           ACCEPT WS-R.
-           COMPUTE WS-TOT = WS-X + WS-Y.                                00060004
-           DISPLAY 'SUM OF ' WS-X ' AND 'WS-Y ' IS:' WS-TOT.            00070001
+           COMPUTE WS-TOT = WS-X + WS-Y.
+           DISPLAY 'SUM OF 'WS-X ' AND 'WS-Y ' IS:'WS-TOT.
            IF WS-TOT NOT = WS-Z THEN
               MOVE +12  TO RETURN-CODE
            ELSE
               MOVE ZERO TO RETURN-CODE.
-           GOBACK.                                                      00080001
+           STOP RUN.
